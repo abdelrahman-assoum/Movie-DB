b0330f18a30756c7e680f8fe7e59acc0dae39da4
abdelrahman <abdelrahman.assoum@gmail.com>
2022-12-30 14:06:53 +0200

    step 3

--- index.js
+++ index.js
@@ -1,8 +1,16 @@
 const express = require('express');
 const app = express();
 const PORT = 3000;
-
+const currentTime = new Date().toLocaleTimeString([], { hour12: false });
 app.get('/', (req, res) => {
     res.send('ok')
   });
+
+app.get('/test', (req, res) => {
+    res.json({status:200, message:"ok"})
+})
+app.get('/time', (req, res) => {
+    res.send({status:200, message:`${currentTime}`});
+  });
+
 app.listen(PORT);
